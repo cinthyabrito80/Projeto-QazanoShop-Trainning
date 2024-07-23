96e89c684f866164432d22844c8a1a0e5d9b7fdd
cinthyabrito80 <cinthya_brito@yahoo.com.br>
2024-07-23 18:48:05 -0300

    add novos arquivos no repositorio

---
+++ src/test/java/support/Utils.java
@@ -0,0 +1,13 @@
+package support;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import runner.RunCucumberTest;
+
+public class Utils extends RunCucumberTest {
+    public void esperarElementoVisivel(By element, int tempo) {
+        WebDriverWait wait = new WebDriverWait(driver,tempo);
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+}
